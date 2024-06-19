41372511663ed82df5b8bbcc6aa674751abc5f0a
Krunal Patel <patel.krunal.tech@gmail.com>
2024-06-19 07:01:48 +0000

    feat: made some changes

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=jun222work/house_pricing
+
+# Step 2:  
+# Authenticate & tag
+docker login -u "jun222work "
+docker tag house_pricing $dockerpath
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
